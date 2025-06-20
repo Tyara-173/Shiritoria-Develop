8013f8294380b09c91fa825d73f55ffbb2fb0849
Tyara-173 <126230950+Tyara-173@users.noreply.github.com>
2025-06-20 15:04:50 +0900

    [add] 素材の追加

--- Assets/Script/MojiCube.cs
+++ Assets/Script/MojiCube.cs
@@ -6,7 +6,13 @@ using TMPro;
 public class MojiCube : MonoBehaviour
 {
     public TMP_Text moji;
+    public Sprite[] backImages;
 
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = backImages[Random.Range(0, backImages.Length)];
+    }
+    
     public void SetMoji(char c)
     {
         moji.SetText(c.ToString());
